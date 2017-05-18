5780e74030838beb3787dd8c66dc803bf7a128b3
JenRob17 <JennRobertson17@gmail.com>
2017-05-17 22:05:57 -0500

    Working webpack config

---
+++ src/app/components/Root.js
@@ -0,0 +1,24 @@
+// import React, { Component } from "react";
+// import { render } from "react-dom";
+//
+// class Root extends Component {
+//     render() {
+//       return (
+//         <div className="container">
+//           <div className="row">
+//             <div className="col-xs-10 col-xs-offset-1">
+//               </div>
+//             </div>
+//             <div className="row">
+//               <div classNAme="col-xs-10 col-xs-offset-1">
+//                   {this.props.children}
+//               </div>
+//             </div>
+//         </div>
+//       );
+//     }
+// }
+//
+// export default Root;
+
+// This.props.children is the Home Component
---
+++ src/app/components/User.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class User extends Component {
+  render() {
+    return (
+        <div>
+          <h3> The User Page </h3>
+          <p> User ID: </p>
+        </div>
+    );
+  }
+}
