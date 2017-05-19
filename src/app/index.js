ddab635db6a550b83433c5c20facc287a969bf53
JenRob17 <JennRobertson17@gmail.com>
2017-05-19 17:04:15 -0500

    resolving merge conflicts

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,8 @@
-console.log("It Works!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Layout from './components/Layout';
+
+ReactDOM.render(
+  <Layout/>,
+  document.getElementById('root')
+);
